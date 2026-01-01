1c81e886bbf87b1c682693b0ceda8917c5e94812
Abdul Rehman <abdul4rehman215@gmail.com>
2026-01-01 11:50:12 +0530

    Create variables.ps1

---
+++ lab02-powershell-fundamentals/scripts/variables.ps1
@@ -0,0 +1,10 @@
+$name = "PowerShell Student"
+$age = 25
+$isLearning = $true
+$fruits = @("Apple","Banana","Orange","Grape")
+
+Write-Output "Name: $name"
+Write-Output "Age: $age"
+Write-Output "Learning PowerShell: $isLearning"
+Write-Output "First fruit: $($fruits[0])"
+Write-Output "All fruits: $fruits"

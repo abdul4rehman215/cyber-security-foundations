f774867d40cbe0b340592ae620f31c1348b587ac
Abdul Rehman <abdul4rehman215@gmail.com>
2026-01-02 10:16:09 +0530

    Create file_encryptor.py

---
+++ lab14-cryptography-with-python/scripts/file_encryptor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes, padding
+from cryptography.hazmat.backends import default_backend
+import os
+import sys
+import getpass
